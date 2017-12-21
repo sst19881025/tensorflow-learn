0074abcff79dfde12c8e57086fd0e00caba186e4
孙世通 <shitong.sun@socialcredits.cn>
2017-12-21 18:14:15 +0800

    update

--- corpus/boson_sdk.py
+++ corpus/boson_sdk.py
@@ -38,7 +38,7 @@ def nlp_seg(text):
     return result
 
 
-def save_data(_id, word, tag):
+def save_data2mongo(_id, word, tag):
     conn = pymongo.MongoClient(['127.0.0.1:27017'])
     db = conn['corpus']
     #pos_tag_text = map(lambda x:' '.join(['%s/%s' % it for it in zip(x['word'], x['tag'])]), pos_tag)
@@ -86,6 +86,58 @@ def save_sentence(db, collection):
     conn.close()
 
 
+def save_text(db, collection):
+    """ 把以文章为个体的分词数据存为单字分割的整个语料文本
+    """
+    result = {}
+    conn = pymongo.MongoClient(['127.0.0.1'])
+    db = conn[db]
+    data = db[collection].find({}, {'word':1, 'tag':1, '_id':0})
+    total_text = ''
+    for i, doc in enumerate(data):
+        doc_text = ''
+        word_list = doc['word']
+        #cur_sentence = []
+        #cur_labels = []
+        for word in word_list:
+            word = word.strip()
+            # 汉子体系
+            if re.search(u'[\u2E80-\u9FFF]', word):
+                # 对每个词（双十一/你好 等）中的每个字给标签
+                word_len = len(word)
+                for j, char in enumerate(word):
+                    # 每个字向量的行拼接
+                    doc_text += char + ' '
+            elif re.search(u'[0-9a-zA-Z\-]', word):
+                doc_text += char + ' '
+        total_text += strQ2B(doc_text).strip() + ' <BR> '
+        if i % 100 == 0:
+            print('doc {} finished'.format(i))
+    w = open('/usr/app/tensorflow-learn/corpus/data/news.txt', 'w')
+    w.write(total_text.encode('utf-8'))
+    w.close()
+    conn.close()
+
+
+def strQ2B(ustring):
+    """ 把字符串全角转半角
+    """
+    ustring = ustring.decode('utf-8') if isinstance(ustring, str) else ustring
+    rstring = ""
+    for uchar in ustring:
+        inside_code=ord(uchar)
+        if inside_code==0x3000:
+            inside_code=0x0020
+        else:
+            inside_code-=0xfee0
+        # 转完之后不是半角字符则返回原来的字符  
+        if inside_code<0x0020 or inside_code>0x7e:
+           rstring+=uchar
+        else:
+            rstring+=(unichr(inside_code))
+    return rstring
+
+
 def main(data_path):
     """ 存储词性标注结果到mongo
     """
@@ -97,7 +149,7 @@ def main(data_path):
         wd_tag_data = nlp_seg(news)[0]
         wd_data = wd_tag_data['word']
         tag_data = wd_tag_data['tag']
-        save_data(_id=name, word=wd_data, tag=tag_data)
+        save_data2mongo(_id=name, word=wd_data, tag=tag_data)
         if i % 10 == 0:
             print('news data {} finished'.format(i))
         #pdb.set_trace()
@@ -109,9 +161,10 @@ def res_text(result):
 
 
 if __name__ == '__main__':
-    data_path = '/usr/app/tensorflow-learn/corpus/data/news/'
-    main(data_path)
-    save_sentence('corpus', 'news')
+    #data_path = '/usr/app/tensorflow-learn/corpus/data/news/'
+    #main(data_path)
+    #save_sentence('corpus', 'news')
+    save_text('corpus', 'news')
 
     #text = '拉卡拉集团装入A股西藏旅游流产后，分拆支付业务单独在创业板上市，融资约20亿元，证监会今日披露了其招股书。 根据招股书，拉卡拉支付股份有限公司（以下简称拉卡拉支付）计划公开发行不超过4001万股，融资规模在20亿元左右，资金计划全部用于第三方支付产业升级项目。中信建设证券股份有限公司是此次公开发行的保荐人。 2016年5月份，拉卡拉集团曾筹划作价110亿元装入A股西藏旅游，但该交易方案出炉后被市场质疑涉嫌刻意规避借壳，上交所一度发出问询函要求重组双方说明此事，最终双方以政策变化等原因终止了交易。 此次计划在创业办上市的拉卡拉支付业务，只是原拉卡拉集团业务的一部分。 2016年10月份，拉卡拉集团曾对外公布业务重组方案：成立拉卡拉控股作为最高层的母公司，下设拉卡拉支付集团和考拉金服集团。 拉卡拉控股董事长孙陶然曾解释重组原因为，一方面是业务发展的需要，另外一方面也是根据监管的要求：拉卡拉支付集团旗下业务是由“一行三会”发牌照的业务，考拉金服集团旗下业务金融办、金融局来监管的业务。 招股书显示，2016年第四季度，拉卡拉支付剥离了主营增值金融业务的北京拉卡拉小贷、广州拉卡拉小贷等10家控股及参股子公司。 目前，拉卡拉支付经营范围为银行卡收单、互联网支付、数字电视支付、预付卡受理、移动电话支付。 拉卡拉支付股权结构图 股权结构方面，拉卡拉支付第一大股东为联想控股，直接持有公司31.38%股份，孙陶然和孙浩然合计直接持股比例为13.06%。 不过，招股书称拉卡拉支付目前无实际控制人。原因为，第一，根据联想控股出具《关于未对拉卡拉支付股份有限公司实施控制的声 明函》，联想控股对拉卡拉支付仅为财务性投资入股，以获取投资收益为目的，不单独或联合谋求对公司的控制；第二，拉卡拉支付的股权结构相对分散，且主要股东之间不存在一致行动安排，故无关联股东能实际支配公司经营。 经营业绩方面，招股书显示，拉卡拉支付2013、2014年度承受着亏损，净利润分别为 -12681.03万元、－19694.7万元，2015年起扭亏为盈，净利润为12356.34万元，2016年1月9日净利润21171.59万元。 2013年至2015年，拉卡拉支付营业收入分别为61707.93万元、91522.84万元、158838.94万元、199398.09万元。 [责编：张帆]'
     #result = nlp_pos_tag(text)
--- corpus/word2vec/word2vec_basic.py
+++ corpus/word2vec/word2vec_basic.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
 # Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,10 +26,12 @@ import math
 import os
 import random
 import zipfile
+import pdb
+import pickle
 
 import numpy as np
 from six.moves import urllib
-from six.moves import xrange  # pylint: disable=redefined-builtin
+from six.moves import xrange    # pylint: disable=redefined-builtin
 import tensorflow as tf
 
 # Step 1: Download the data.
@@ -34,58 +39,63 @@ url = 'http://mattmahoney.net/dc/'
 
 
 def maybe_download(filename, expected_bytes):
-  """Download a file if not present, and make sure it's the right size."""
-  if not os.path.exists(filename):
-    filename, _ = urllib.request.urlretrieve(url + filename, filename)
-  statinfo = os.stat(filename)
-  if statinfo.st_size == expected_bytes:
-    print('Found and verified', filename)
-  else:
-    print(statinfo.st_size)
-    raise Exception(
-        'Failed to verify ' + filename + '. Can you get to it with a browser?')
-  return filename
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+                'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
 
 filename = maybe_download('text8.zip', 31344016)
 
 
 # Read the data into a list of strings.
 def read_data(filename):
-  """Extract the first file enclosed in a zip file as a list of words."""
-  with zipfile.ZipFile(filename) as f:
-    data = tf.compat.as_str(f.read(f.namelist()[0])).split()
-  return data
+    """Extract the first file enclosed in a zip file as a list of words."""
+    with zipfile.ZipFile(filename) as f:
+        data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+    return data
+
+
+def save_data(data):
+    w = open('./embedding.pkl', 'w')
+    pickle.dumps(data, w)
+    w.close()
+
 
 vocabulary = read_data(filename)
-print('Data size', len(vocabulary))
+print('Data size %s, vocab len %s' % (len(vocabulary), len(set(vocabulary))))
 
 # Step 2: Build the dictionary and replace rare words with UNK token.
 vocabulary_size = 50000
 
-
 def build_dataset(words, n_words):
-  """Process raw inputs into a dataset."""
-  count = [['UNK', -1]]
-  count.extend(collections.Counter(words).most_common(n_words - 1))
-  dictionary = dict()
-  for word, _ in count:
-    dictionary[word] = len(dictionary)
-  data = list()
-  unk_count = 0
-  for word in words:
-    if word in dictionary:
-      index = dictionary[word]
-    else:
-      index = 0  # dictionary['UNK']
-      unk_count += 1
-    data.append(index)
-  count[0][1] = unk_count
-  reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
-  return data, count, dictionary, reversed_dictionary
+    """Process raw inputs into a dataset."""
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(n_words - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0    # dictionary['UNK']
+            unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reversed_dictionary
 
-data, count, dictionary, reverse_dictionary = build_dataset(vocabulary,
-                                                            vocabulary_size)
-del vocabulary  # Hint to reduce memory.
+data, count, dictionary, reverse_dictionary = build_dataset(vocabulary, vocabulary_size)
+del vocabulary    # Hint to reduce memory.
 print('Most common words (+UNK)', count[:5])
 print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
 
@@ -93,171 +103,197 @@ data_index = 0
 
 # Step 3: Function to generate a training batch for the skip-gram model.
 def generate_batch(batch_size, num_skips, skip_window):
-  global data_index
-  assert batch_size % num_skips == 0
-  assert num_skips <= 2 * skip_window
-  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
-  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
-  span = 2 * skip_window + 1  # [ skip_window target skip_window ]
-  buffer = collections.deque(maxlen=span)
-  if data_index + span > len(data):
-    data_index = 0
-  buffer.extend(data[data_index:data_index + span])
-  data_index += span
-  for i in range(batch_size // num_skips):
-    target = skip_window  # target label at the center of the buffer
-    targets_to_avoid = [skip_window]
-    for j in range(num_skips):
-      while target in targets_to_avoid:
-        target = random.randint(0, span - 1)
-      targets_to_avoid.append(target)
-      batch[i * num_skips + j] = buffer[skip_window]
-      labels[i * num_skips + j, 0] = buffer[target]
-    if data_index == len(data):
-      buffer[:] = data[:span]
-      data_index = span
-    else:
-      buffer.append(data[data_index])
-      data_index += 1
-  # Backtrack a little bit to avoid skipping words in the end of a batch
-  data_index = (data_index + len(data) - span) % len(data)
-  return batch, labels
+    """
+    从文本总体的第二个单词开始，每个单词依次作为输入，它的输出可以是上下文范围内的单词中的任何一个单词。
+    一般不是取全部而是随机取其中的几组，以增加随机性。
+    - batch_size: 就是每次训练用多少数据，
+    - skip_window: 是确定取一个词周边多远的词来训练， # How many words to consider left and right.
+    - num_skips: 对于一个输入数据，产生多少个标签数据。How many times to reuse an input to generate a label. 
+    所以skip_window决定上下文的长度，就是当前词的周围多少个词内的词被视为它的上下文的范围内，
+    然后从这上下文范围内的词中随机取num_skips个与输入组合成num_skips组训练数据。这些都是正样本。
+    """
+    global data_index
+    assert batch_size % num_skips == 0
+    assert num_skips <= 2 * skip_window
+    batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    span = 2 * skip_window + 1    # [ skip_window target skip_window ]
+    # 这个很重要，最大长度是span，后面如果数据超过这个长度，前面的会被挤掉，
+    # 这样使得buffer里面永远是data_index周围的span歌数据，
+    # 而buffer的第skip_window个数据永远是当前处理循环里的输入数据
+    buffer = collections.deque(maxlen=span)
+    if data_index + span > len(data):
+        data_index = 0
+    buffer.extend(data[data_index:data_index + span])
+    data_index += span
+
+    for i in range(batch_size // num_skips):    # batch_size = num_skips * lopp_cnt
+        target = skip_window    # target label at the center of the buffer
+        targets_to_avoid = [skip_window]
+        # loop for num_skips(目标词的左右)
+        for j in range(num_skips):
+            # 在buffer中采样，以中心位置为X，周围随机位置为Y
+            while target in targets_to_avoid:
+                # 随机取一个，增强随机性，减少训练时进入局部最优解
+                target = random.randint(0, span - 1)
+            targets_to_avoid.append(target)
+            # 这里保存的是训练的输入序列
+            batch[i * num_skips + j] = buffer[skip_window]
+            # 这里保存的是训练时的输出序列，也就是标签
+            labels[i * num_skips + j, 0] = buffer[target]
+        # 超长时回到开始, append时会把queue的开始的几个挤掉
+        if data_index == len(data):
+            for word in data[:span]:
+                buffer.append(data)
+            data_index = span
+        else:
+            buffer.append(data[data_index])
+            data_index += 1
+    # Backtrack a little bit to avoid skipping words in the end of a batch
+    data_index = (data_index + len(data) - span) % len(data)
+    return batch, labels
 
 batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
 for i in range(8):
-  print(batch[i], reverse_dictionary[batch[i]],
-        '->', labels[i, 0], reverse_dictionary[labels[i, 0]])
+    print(batch[i], reverse_dictionary[batch[i]],
+                '->', labels[i, 0], reverse_dictionary[labels[i, 0]])
 
 # Step 4: Build and train a skip-gram model.
 
 batch_size = 128
-embedding_size = 128  # Dimension of the embedding vector.
-skip_window = 1       # How many words to consider left and right.
-num_skips = 2         # How many times to reuse an input to generate a label.
+embedding_size = 128    # Dimension of the embedding vector.
+skip_window = 1             # How many words to consider left and right.
+num_skips = 2                 # How many times to reuse an input to generate a label.
 
 # We pick a random validation set to sample nearest neighbors. Here we limit the
 # validation samples to the words that have a low numeric ID, which by
 # construction are also the most frequent.
-valid_size = 16     # Random set of words to evaluate similarity on.
-valid_window = 100  # Only pick dev samples in the head of the distribution.
+valid_size = 16         # Random set of words to evaluate similarity on.
+valid_window = 100    # Only pick dev samples in the head of the distribution.
+# 从[0:valid_window]中随机选valid_size个数据, 构成一维数组, replace=False表示不重复
 valid_examples = np.random.choice(valid_window, valid_size, replace=False)
-num_sampled = 64    # Number of negative examples to sample.
+num_sampled = 64        # Number of negative examples to sample.
 
 graph = tf.Graph()
 
 with graph.as_default():
 
-  # Input data.
-  train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
-  train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
-  valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+    # Input data.
+    train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+    train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+    valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
-  # Ops and variables pinned to the CPU because of missing GPU implementation
-  with tf.device('/cpu:0'):
-    # Look up embeddings for inputs.
-    embeddings = tf.Variable(
-        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-    embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+    # Ops and variables pinned to the CPU because of missing GPU implementation
+    with tf.device('/cpu:0'):
+        # Look up embeddings for inputs.
+        embeddings = tf.Variable(
+                tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+        embed = tf.nn.embedding_lookup(embeddings, train_inputs)
 
-    # Construct the variables for the NCE loss
-    nce_weights = tf.Variable(
-        tf.truncated_normal([vocabulary_size, embedding_size],
-                            stddev=1.0 / math.sqrt(embedding_size)))
-    nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+        # Construct the variables for the NCE loss
+        nce_weights = tf.Variable(
+                tf.truncated_normal([vocabulary_size, embedding_size], stddev=1.0 / math.sqrt(embedding_size)))
+        nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
 
-  # Compute the average NCE loss for the batch.
-  # tf.nce_loss automatically draws a new sample of the negative labels each
-  # time we evaluate the loss.
-  loss = tf.reduce_mean(
-      tf.nn.nce_loss(weights=nce_weights,
-                     biases=nce_biases,
-                     labels=train_labels,
-                     inputs=embed,
-                     num_sampled=num_sampled,
-                     num_classes=vocabulary_size))
+    # Compute the average NCE loss for the batch.
+    # tf.nce_loss automatically draws a new sample of the negative labels each
+    # time we evaluate the loss.
+    loss = tf.reduce_mean( # 求均值
+            tf.nn.nce_loss(weights=nce_weights,
+                                         biases=nce_biases,
+                                         labels=train_labels,
+                                         inputs=embed,
+                                         num_sampled=num_sampled,
+                                         num_classes=vocabulary_size))
 
-  # Construct the SGD optimizer using a learning rate of 1.0.
-  optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+    # Construct the SGD optimizer using a learning rate of 1.0.
+    optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
 
-  # Compute the cosine similarity between minibatch examples and all embeddings.
-  norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
-  normalized_embeddings = embeddings / norm
-  valid_embeddings = tf.nn.embedding_lookup(
-      normalized_embeddings, valid_dataset)
-  similarity = tf.matmul(
-      valid_embeddings, normalized_embeddings, transpose_b=True)
+    # Compute the cosine similarity between minibatch examples and all embeddings.
+    norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+    # shape: vocabulary_size * embedding_size
+    normalized_embeddings = embeddings / norm
+    # shape: valid_size * embedding_size
+    valid_embeddings = tf.nn.embedding_lookup(
+            normalized_embeddings, valid_dataset)
+    # shape: valid_size * vocabulary_size
+    similarity = tf.matmul(
+            valid_embeddings, normalized_embeddings, transpose_b=True)
 
-  # Add variable initializer.
-  init = tf.global_variables_initializer()
+    # Add variable initializer.
+    init = tf.global_variables_initializer()
 
 # Step 5: Begin training.
 num_steps = 100001
 
 with tf.Session(graph=graph) as session:
-  # We must initialize all variables before we use them.
-  init.run()
-  print('Initialized')
+    # We must initialize all variables before we use them.
+    init.run()
+    print('Initialized')
 
-  average_loss = 0
-  for step in xrange(num_steps):
-    batch_inputs, batch_labels = generate_batch(
-        batch_size, num_skips, skip_window)
-    feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+    average_loss = 0
+    for step in xrange(num_steps):
+        # shape: (batch_size), (batch_size, 1)
+        batch_inputs, batch_labels = generate_batch(
+                batch_size, num_skips, skip_window)
+        feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
 
-    # We perform one update step by evaluating the optimizer op (including it
-    # in the list of returned values for session.run()
-    _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
-    average_loss += loss_val
+        # We perform one update step by evaluating the optimizer op (including it
+        # in the list of returned values for session.run()
+        _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
+        average_loss += loss_val
 
-    if step % 2000 == 0:
-      if step > 0:
-        average_loss /= 2000
-      # The average loss is an estimate of the loss over the last 2000 batches.
-      print('Average loss at step ', step, ': ', average_loss)
-      average_loss = 0
+        if step % 2000 == 0:
+            if step > 0:
+                average_loss /= 2000
+            # The average loss is an estimate of the loss over the last 2000 batches.
+            print('Average loss at step ', step, ': ', average_loss)
+            average_loss = 0
 
-    # Note that this is expensive (~20% slowdown if computed every 500 steps)
-    if step % 10000 == 0:
-      sim = similarity.eval()
-      for i in xrange(valid_size):
-        valid_word = reverse_dictionary[valid_examples[i]]
-        top_k = 8  # number of nearest neighbors
-        nearest = (-sim[i, :]).argsort()[1:top_k + 1]
-        log_str = 'Nearest to %s:' % valid_word
-        for k in xrange(top_k):
-          close_word = reverse_dictionary[nearest[k]]
-          log_str = '%s %s,' % (log_str, close_word)
-        print(log_str)
-  final_embeddings = normalized_embeddings.eval()
-
+        # Note that this is expensive (~20% slowdown if computed every 500 steps)
+        if step % 10000 == 0:
+            # shape: valid_size * vocabulary_size
+            sim = similarity.eval()
+            for i in xrange(valid_size):
+                valid_word = reverse_dictionary[valid_examples[i]]
+                top_k = 8    # number of nearest neighbors
+                # numpy.argsort()获取从小到大排序后的下标, 则-sim[i, :], 就是相关度从高到低的下标
+                nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+                log_str = 'Nearest to %s:' % valid_word
+                for k in xrange(top_k):
+                    close_word = reverse_dictionary[nearest[k]]
+                    log_str = '%s %s,' % (log_str, close_word)
+                print(log_str)
+    final_embeddings = normalized_embeddings.eval()
+    save_data(final_embeddings)
 # Step 6: Visualize the embeddings.
 
 
 def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
-  assert low_dim_embs.shape[0] >= len(labels), 'More labels than embeddings'
-  plt.figure(figsize=(18, 18))  # in inches
-  for i, label in enumerate(labels):
-    x, y = low_dim_embs[i, :]
-    plt.scatter(x, y)
-    plt.annotate(label,
-                 xy=(x, y),
-                 xytext=(5, 2),
-                 textcoords='offset points',
-                 ha='right',
-                 va='bottom')
+    assert low_dim_embs.shape[0] >= len(labels), 'More labels than embeddings'
+    plt.figure(figsize=(18, 18))    # in inches
+    for i, label in enumerate(labels):
+        x, y = low_dim_embs[i, :]
+        plt.scatter(x, y)
+        plt.annotate(label,
+                     xy=(x, y),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
 
-  plt.savefig(filename)
+    plt.savefig(filename)
 
 try:
-  # pylint: disable=g-import-not-at-top
-  from sklearn.manifold import TSNE
-  import matplotlib.pyplot as plt
+    # pylint: disable=g-import-not-at-top
+    from sklearn.manifold import TSNE
+    import matplotlib.pyplot as plt
 
-  tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000, method='exact')
-  plot_only = 500
-  low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
-  labels = [reverse_dictionary[i] for i in xrange(plot_only)]
-  plot_with_labels(low_dim_embs, labels)
+    tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000, method='exact')
+    plot_only = 500
+    low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
+    labels = [reverse_dictionary[i] for i in xrange(plot_only)]
+    plot_with_labels(low_dim_embs, labels)
 
 except ImportError:
-  print('Please install sklearn, matplotlib, and scipy to show embeddings.')
+    print('Please install sklearn, matplotlib, and scipy to show embeddings.')
